c77d55feb5a9f1d1b017a00c15dd53e7182ab523
Ahmedtaha249 <taha.ahmedali445@gmail.com>
2021-09-05 08:46:32 -0700

    solved part 2

--- part_2/part_2.js
+++ part_2/part_2.js
@@ -5,32 +5,47 @@ PART 3 [General Information]:
 Type the correct answer below each question:
 
 Q1) Why do we use databases?
-
+ To save information and order informatin easly to get information any time
 
 Q2) What are JWTs used for?.
-
+ jwt (json web token) to create token in login and authunteciation information senstive
 
 Q3) What are the main three sections of a JWT?
-
+ 1-headers
+ 2-payloads
+ 3-signture
 
 Q4) Name three types of express middle-wares.
-
+1- error handling
+2-application level ware
+3- application built morgan
 
 Q5) What is the `effect` hook used for in react?
-``
+`` 
+more dynmic ,speed 
+
 
 Q6) What is the `state` hook used for in react?
-
+  it is const[varible,setVarbile]=useState()
 
 Q7) List three thing you can use to create a responsive web application.
-
+ 1-react.js
+ 2-node js
+ react hooks
 
 Q8) Name two of mongoose middle-wares and explain what they are used for.
-
+1-authnetication: process  sure information
+2-authorization: pprocess in permission
 
 Q9) What is the difference between authentication and authorization?
-
+1-authnetication: process  sure information      هي عملية التاكد من المعلومات الموجودة
+2-authorization: pprocess in permission   example delete, update,create                               هي عملية التاكد من الصلاحيات 
 
 Q10) Why do we save some information as an environment variable (in .env file)?
+ because to stop share and access to information senstive example email ,password ,email ,name
+ تستخدم لمنع نشر اي من المعلومات الحساسة او الوصول اليها 
+
+const dotenv=require("dotenv")
+
 
 */
